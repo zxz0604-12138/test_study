c3816ae81cbc8292c156def2cd6bc3e4a4e261a9
zxz <2780199368@qq.com>
2025-04-12 16:25:10 +0800

    xx

---
+++ src/main/java/org/example/test_study/TestStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.test_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/test_study/TestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.test_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
